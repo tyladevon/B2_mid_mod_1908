04cae3179124b1fddf69c1275a04d02295e42dea
Tyla Devon <tyladevon@gmail.com>
2019-10-18 12:13:20 -0600

    Add model test for Instructor. Test for name and relationship w/students

---
+++ spec/models/instructor_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Instructor, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+  end
+
+  describe "relationships" do
+    it {should have_many :students}
+  end
+
+end
