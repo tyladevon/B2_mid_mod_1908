0411aeeb0d0a61bd96b65b73ebfb2aeec5bcbffa
Tyla Devon <tyladevon@gmail.com>
2019-10-19 16:20:39 -0600

    Add Model tests for Instructor and Student models

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe Student, type: :model do
   describe "validations" do
   it {should validate_presence_of :name }
-  it {should validate_presence_of :cohort_number }
+  it {should validate_presence_of :cohort }
   it {should validate_presence_of :age }
   end
 
   describe "relationships" do
-    it {should have_many :students}
+    it {should have_many :instructor_students}
+    it {should have_many :instructors}
   end
 end
