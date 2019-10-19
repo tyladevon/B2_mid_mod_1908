fbb2c16b98ced9f350e94b2465b4418370c0b6d8
Tyla Devon <tyladevon@gmail.com>
2019-10-19 16:27:28 -0600

    TDD-MVC-User Story 1 and 2

---
+++ db/migrate/20191019203329_create_instructor_students.rb
@@ -0,0 +1,8 @@
+class CreateInstructorStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :instructor_students do |t|
+      t.references :instructor, foreign_key: true
+      t.references :student, foreign_key: true
+    end
+  end
+end
